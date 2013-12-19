5ede69273f19e37d8ae723c9573d594a37c64da3
Eric Dill <eddill@ncsu.edu>
2013-12-19 16:22:30 -0500

    Added copyright header to all files

--- src/dataFileTypes/ImagePropertiesViewer.java
+++ src/dataFileTypes/ImagePropertiesViewer.java
@@ -1,3 +1,14 @@
+/*******************************************************************************
+ * Copyright (c) 2013 Eric Dill -- eddill@ncsu.edu. North Carolina State University. All rights reserved.
+ * This program and the accompanying materials
+ * are made available under the terms of the GNU Public License v3.0
+ * which accompanies this distribution, and is available at
+ * http://www.gnu.org/licenses/gpl.html
+ * 
+ * Contributors:
+ *     Eric Dill -- eddill@ncsu.edu - initial API and implementation
+ * 	James D. Martin -- jdmartin@ncsu.edu - Principal Investigator
+ ******************************************************************************/
 package dataFileTypes;
 
 import java.awt.GridLayout;
